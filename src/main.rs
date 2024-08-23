15fb67fa16514fafb6182d82cbd08a52ae20b5d6
Connor Slade <connor@connorcode.com>
2024-08-23 18:45:40 -0400

    Cleanup demodulator

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,50 @@
+use std::io::{stdout, Write};
+
 use hound::{SampleFormat, WavSpec, WavWriter};
+use itertools::Itertools;
 use num_complex::Complex;
+
+const BUFFER_SIZE: usize = 16 * 16384;
+const SAMPLE_RATE: u32 = 250_000;
 
 fn main() {
     let mut device = rtlsdr::open(0).unwrap();
 
     device.set_center_freq(99_100_000).unwrap();
-    device.set_sample_rate(250_000).unwrap();
+    device.set_sample_rate(SAMPLE_RATE).unwrap();
     device.reset_buffer().unwrap();
 
     let mut wav = WavWriter::create(
-        "out-nofilter.wav",
+        "out.wav",
         WavSpec {
             channels: 1,
             // sample_rate: 44100,
-            sample_rate: 250_000,
+            sample_rate: SAMPLE_RATE,
             bits_per_sample: 32,
             sample_format: SampleFormat::Float,
         },
     )
     .unwrap();
 
-    for i in 0..100 {
-        println!("{i}/100 - {}%", i + 1);
+    let mut time = 0.0;
+    loop {
+        print!("\r{time:.1}s");
+        stdout().flush().unwrap();
 
-        let data = device.read_sync(16 * 16384).unwrap();
-        let iq = data
-            .chunks_exact(2)
-            .map(|chunk| Complex::new(chunk[0] as f32 / 128.0 - 1.0, chunk[1] as f32 / 128.0 - 1.0))
-            .collect::<Vec<_>>();
-        let pcm = iq.windows(2).map(|window| {
-            let c = window[0] * window[1].conj();
+        time += BUFFER_SIZE as f32 / SAMPLE_RATE as f32 / 2.0;
+
+        let data = device.read_sync(BUFFER_SIZE).unwrap();
+        let iq = data.chunks_exact(2).map(|chunk| {
+            Complex::new(chunk[0] as f32 / 128.0 - 1.0, chunk[1] as f32 / 128.0 - 1.0)
+        });
+        let pcm = iq.tuple_windows().map(|(i, q)| {
+            let c = i * q.conj();
             let angle = f32::atan2(c.im as f32, c.re as f32);
             angle * 0.4
         });
-
-        // let rc = (150_000.0 * 2.0 * PI).recip();
-        // let dt = (250_000_f32).recip();
-        // let alpha = dt / (rc + dt);
-
-        // let mut filtered = vec![0.0; data.len() / 2];
-        // filtered[0] = pcm.next().unwrap();
-        // for (i, sample) in pcm.enumerate() {
-        //     filtered[i + 1] = filtered[i] + alpha * (sample - filtered[i]);
-        // }
-
-        // for sample in filtered {
-        //     wav.write_sample(sample).unwrap();
-        // }
 
         for sample in pcm {
             wav.write_sample(sample).unwrap();
         }
     }
-
-    wav.finalize().unwrap();
 }
