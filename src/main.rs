f1a2d3d767c3f285b39d484b91f2e4ee0a47899d
Connor Slade <connor@connorcode.com>
2024-08-24 17:54:07 -0400

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ const BUFFER_SIZE: usize = 16_384;
 const SAMPLE_RATE: u32 = 250_000;
 const AUDIO_CUTOFF_FREQ: f32 = 15_000.0;
 
-const SQUELCH: f32 = 0.6;
+const SQUELCH: f32 = 0.7;
 
 const WAVE_SAMPLE_RATE: u32 = 44_100;
 const WAVE_SPEC: WavSpec = WavSpec {
@@ -33,11 +33,6 @@ struct Message {
 fn main() {
     let mut device = rtlsdr::open(0).unwrap();
 
-    println!("Gains: {:?}", device.get_tuner_gains().unwrap());
-
-    device.set_tuner_gain_mode(true).unwrap();
-    device.set_tuner_gain(364).unwrap();
-
     device.set_center_freq(156_450_000).unwrap();
     device.set_sample_rate(SAMPLE_RATE).unwrap();
     device.reset_buffer().unwrap();
@@ -49,15 +44,17 @@ fn main() {
         let data = device.read_sync(BUFFER_SIZE).unwrap();
         let iq = data
             .chunks_exact(2)
-            .map(|chunk| Complex::new(chunk[0] as f32 / 128.0 - 1.0, chunk[1] as f32 / 128.0 - 1.0))
+            .map(|chunk| Complex::new(chunk[0] as f32 / 127.5 - 1.0, chunk[1] as f32 / 127.5 - 1.0))
             .collect::<Vec<_>>();
 
         if rms(&iq) < SQUELCH {
             if let Some(Message { wav, buffer }) = wav.take() {
                 wav.finalize().unwrap();
-                let start = Instant::now();
-                let text = transcriber.transcribe(&buffer).unwrap();
-                println!("{text} ({:?})", start.elapsed());
+                if !buffer.is_empty() {
+                    let start = Instant::now();
+                    let text = transcriber.transcribe(&buffer).unwrap();
+                    println!("{text} ({:?})", start.elapsed());
+                }
             }
             continue;
         }
@@ -72,12 +69,12 @@ fn main() {
 
         let pcm = iq
             .into_iter()
-            .low_pass(SAMPLE_RATE, 100_000.0)
+            .low_pass(SAMPLE_RATE, 20_000.0)
             .tuple_windows()
             .map(|(i, q)| {
                 let c = i * q.conj();
                 let angle = f32::atan2(c.im as f32, c.re as f32);
-                angle * 2.0
+                angle
             });
 
         let audio = pcm
